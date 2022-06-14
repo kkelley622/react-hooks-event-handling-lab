dad41b6b12c855997a5aa7e3cb6fbbb314bd4da0
Kevin Kelley <kkelley622@gmail.com>
2022-06-14 09:34:25 -0500

    commit tree c3547cf9674b15d162013b94d945593c973a0830

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from "React";
+function change() {console.log('Entering password...')}
+
+function Keypad() {
+    return <input type= "password" onChange={change}></input>
+    
+    ;
+}
+
+export default Keypad;
